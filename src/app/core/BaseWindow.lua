24b650204e253eafb55045cc65d8d302b16f77a8
AstaTus <chenjunqi5@126.com>
2015-08-27 16:06:37 +0800

    window and config

--- src/app/core/BaseWindow.lua
+++ src/app/core/BaseWindow.lua
@@ -1,4 +1,5 @@
 local BaseWindow = class("BaseWindow")
+BaseWindow.root_ = nil
 
 function BaseWindow:ctor()
 
@@ -21,6 +22,26 @@ function BaseWindow:load()
 end
 
 function BaseWindow:unload()
+end
+
+function BaseWindow:seekNodeByName(root, name)
+    if root == nil then
+		return nil
+    end
+
+	if root:getName() == name then
+		return root
+    end
+
+	local nodes = root:getChildren();
+	for node in nodes do
+        local result = self:seekFromRootByName(node, name);
+		if result ~= nil then
+            return result
+        end
+	end
+
+	return nil    
 end
 
 return BaseWindow
--- src/app/data/ConfigManager.lua
+++ src/app/data/ConfigManager.lua
@@ -3,6 +3,11 @@ local ConfigManager = class("ConfigManager")
 ConfigManager.singleton_ = nil
 ConfigManager.parse_callbacks_ = nil
 
+
+function ConfigManager:ctor()
+    self:initParseCallbacks()
+end
+
 function ConfigManager:getSingleton()
     if self.singleton_ == nil then
         self.singleton_ = ConfigManager.new()
@@ -15,6 +20,15 @@ function ConfigManager:getConfigSize()
     return #self.parse_callbacks_
 end
 
-function ConfigManager:ge
+function ConfigManager:parseConfig(index)
+    self.parse_callbacks_[index]()
+end
+
+function ConfigManager:initParseCallbacks()
+    self.parse_callbacks_[#self.parse_callbacks_ + 1] = function() self:parseWindowConfig() end
+end
+
+function ConfigManager:parseWindowConfig()
+end
 
 return ConfigManager
