0431fe71c46bca9943240d51884c140d875e8fd8
AstaTus <chenjunqi5@126.com>
2015-08-31 00:30:54 +0800

    project file

---
+++ copy.bat
@@ -0,0 +1,5 @@
+rd /s /q D:\Cocos2dxLua3_7\simulator\win32\src
+rd /s /q D:\Cocos2dxLua3_7\simulator\win32\res
+
+echo d |xcopy D:\Cocos2dxLua3_7\src D:\Cocos2dxLua3_7\simulator\win32\src /c/e/q
+echo d |xcopy D:\Cocos2dxLua3_7\res D:\Cocos2dxLua3_7\simulator\win32\res /c/e/q 
